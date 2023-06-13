c5c914a50b47b4576530726e2309cd6bcdd6056c
vishal267 <vishal267@ymail.com>
2023-06-13 20:17:30 +0530

    Update main.tf

--- terraform/resources/eks/main.tf
+++ terraform/resources/eks/main.tf
@@ -213,6 +213,20 @@ module "eks" {
 
 }
 
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
+
 provider "kubernetes" {
   experiments {
     manifest_resource = true
