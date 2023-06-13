4e1d0ee7c4e084abedca13acd554444aafe1e8c4
vishal267 <vishal267@ymail.com>
2023-06-13 19:52:26 +0530

    Update data.tf

--- terraform/resources/eks/data.tf
+++ terraform/resources/eks/data.tf
@@ -6,3 +6,17 @@ data "terraform_remote_state" "network" {
     region = "us-east-1"
   }
 }
+
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
