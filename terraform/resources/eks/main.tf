0885643ccb0f01b2f478c63ed14216eda0b9852e
vishal267 <vishal267@ymail.com>
2023-06-13 19:54:48 +0530

    Update main.tf

--- terraform/resources/eks/main.tf
+++ terraform/resources/eks/main.tf
@@ -1,14 +1,3 @@
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
-  }
-}
 module "eks" {
   source  = "registry.terraform.io/terraform-aws-modules/eks/aws"
   version = "19.10.0"
